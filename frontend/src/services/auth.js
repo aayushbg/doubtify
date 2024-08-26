e33c378938c795134f2110b6d58bee9d3b38af46
Aayush Bhovanbhai Gondaliya <67059275+aayushbg@users.noreply.github.com>
2024-08-27 00:28:27 +0530

    Update auth.js

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://doubtify-backend.onrender.com';
 
 export const register = async (username, email, password) => {
   try {
