4d5b7b396cab737bcc79e86c31bf179299856fdc
Aayush Bhovanbhai Gondaliya <67059275+aayushbg@users.noreply.github.com>
2024-08-27 10:06:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: ['https://doubtify-frontend.onrender.com', 'https://doubtify.vercel.app/', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
